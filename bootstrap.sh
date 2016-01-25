9dd90aa67fecf20e177fd7118df610b5f626a5bc
Ander Elexpuru <trazart@gmail.com>
2016-01-25 09:41:49 +0000

    commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master
 
 function doIt() {
-  rsync $1 --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+  rsync $1 --exclude ".git/" --exclude ".DS_Store" \
+    --exclude "bootstrap.sh" --exclude "brew.sh" \
+    --exclude ".osx" --exclude ".dir_colors" --exclude ".bashrc" \
     --exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude samples -avh --no-perms . ~/
 }
 
@@ -13,7 +16,7 @@ if [ "$1" == "--dry-run" -o "$1" == "-n" ]; then
 
 elif [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
-  ~/brew.sh
+  ./brew.sh
   source ~/.bash_profile
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
